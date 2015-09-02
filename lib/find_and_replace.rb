4b70d410f5378e393978838fe653fcce7944f406
Alexander S. Dickinson <alexandersdickinson@gmail.com>
2015-09-01 20:38:30 -0700

    Find_and_replace function works.

--- lib/find_and_replace.rb
+++ lib/find_and_replace.rb
@@ -1,9 +1,22 @@
 class String
   def find_and_replace(find, replace)
-    words = self.split()
-    words.each_index() do |i|
-      words[i] = replace if words[i] == find
+    sentence = self.split('')
+    find_length = find.length()
+    find_chars = find.split('')
+    replace_chars = replace.split('')
+    sentence.each_index do |i|
+      if sentence[i] == find_chars[0]
+        comparison = (sentence.slice(i, find_length)).join('')
+        if comparison == find
+          sentence.slice!(i, find_length)
+          (1..replace.length()).each do |j|
+            sentence.insert(i, replace_chars[(replace.length() - j)])
+          end
+        else
+        end
+      else
+      end
     end
-    words.join(' ')
+    return sentence.join('')
   end
 end
--- spec/find_and_replace_spec.rb
+++ spec/find_and_replace_spec.rb
@@ -5,4 +5,8 @@ describe('String#find_and_replace') do
   it('finds and replaces whole words') do
     expect('The cow jumped over the moon.'.find_and_replace('cow', 'elephant')).to(eq('The elephant jumped over the moon.'))
   end
+  
+  it('replaces partial matches') do
+    expect('The cat watched the cathode ray tube.'.find_and_replace('cat', 'dog')).to(eq('The dog watched the doghode ray tube.'))
+  end
 end
